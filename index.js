351190d9c135aed5360be80a800428314791e3bc
safiislam <118913244+safiislam@users.noreply.github.com>
2023-06-09 00:39:23 +0600

    set instructor role

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.yrhbvyy.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -62,6 +62,30 @@ async function run() {
       const result = await usersCollection.find().toArray();
       res.send(result);
     });
+
+    app.patch('/users/admin/:id', async(req,res)=>{
+      const id = req.params.id;
+      const  query = {_id: new ObjectId(id)}
+      const option = {
+        $set:{
+          role:'admin'
+        }
+      } 
+      const result = await usersCollection.updateOne(query,option)
+
+    })
+    app.patch('/users/instructor/:id',async (req,res)=>{
+      const id = req.params.id
+      const query = {_id: new ObjectId(id)}
+      const options ={
+        $set:{
+          role:'instructor'
+        }
+      }
+      const result = await usersCollection.updateOne(query,options)
+      res.send(result)
+    })
+
 
     // await client.connect();
     // Send a ping to confirm a successful connection
