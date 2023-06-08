1b8a2bbdba18585d167b486a35f70a0dc4dd0602
safiislam <118913244+safiislam@users.noreply.github.com>
2023-06-08 17:21:26 +0600

    add course

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -9,10 +9,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri =
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.yrhbvyy.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.yrhbvyy.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -25,31 +23,45 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    const coursesCollection = client.db('summary-school').collection('courses')
-    const usersCollection = client.db('summary-school').collection('users')
+    const coursesCollection = client.db("summary-school").collection("courses");
+    const usersCollection = client.db("summary-school").collection("users");
 
-    app.post('/jwt', (req,res)=>{
-      const user = req.body 
-      console.log(user)
-      const token = jwt.sign(user,process.env.JWT_SECRET_KEY,{ expiresIn: '1h' })
-      res.send({token})
-    })
-    // user api 
-    app.post('/users',async(req,res)=>{
-      const user = req.body ;
-     
-      const query = {email: user.email}
-      const isExist = await usersCollection.findOne(query)
-      if(isExist){
-        return res.send({error: 'already exist'})
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const token = jwt.sign(user, process.env.JWT_SECRET_KEY, {
+        expiresIn: "1h",
+      });
+      res.send({ token });
+    });
+
+    // courses api
+    app.get("/course", async (req, res) => {
+      const query = {};
+      const options = {
+        sort: { "totalEnroll" : -1 },
+      };
+      const result = await coursesCollection.find(query, options).toArray();
+      res.send(result);
+      // console.log(result)
+    });
+
+    // user api
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+
+      const query = { email: user.email };
+      const isExist = await usersCollection.findOne(query);
+      if (isExist) {
+        return res.send({ error: "already exist" });
       }
-      const result = await usersCollection.insertOne(user)
-      res.send(result)
-    })
-    app.get('/users',async (req,res)=>{
-      const result = await usersCollection.find().toArray()
-      res.send(result)
-    })
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
 
     // await client.connect();
     // Send a ping to confirm a successful connection
